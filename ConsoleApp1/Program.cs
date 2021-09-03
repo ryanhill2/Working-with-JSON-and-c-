74ef280cd8a01592823e9c7d820a5962bb8fb3c1
ryan.hill <ryan.hill1@mydit.ie>
2021-09-03 11:32:32 +0100

    working with json in c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LoadJson();
+            
+        }
+        public static void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("../../../data.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+
+                foreach (var i in items)
+                {
+                    Console.WriteLine(i.vcc);
+                }
+                
+            }
+                
+            
+        }
+        
+        public class Item
+        {
+            public int millis;
+            public string stamp;
+            public DateTime datetime;
+            public string light;
+            public float temp;
+            public float vcc;
+        }
+    }
+}
